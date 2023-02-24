869f1d4910733999b28709d6dc0b418bb28f32c5
Ronny <4519142+rnnyrk@users.noreply.github.com>
2023-02-24 19:22:07 +0100

    Add textual content to resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -11,14 +11,22 @@ const Resources = () => {
     <>
       <Container className="px-8 my-64">
         <Heading>Notion</Heading>
-        <p className="mb-8">
-          This is my collections of saves from my Notion library. Helpful links, articles etc.
+        <p className="my-8 text-lg">
+          I use Notion basically as my second brain. When I come across an interesting (code
+          related) tweet, see an useful code example or gather new knowdledge through an article I
+          save it to my Notion library.{' '}
+          <strong>
+            <em>A small insight into what's on my mind.</em>
+          </strong>
         </p>
       </Container>
 
       <section className="min-h-screen py-40 bg-slate-900">
         <Container className="px-8">
-          <Heading className="mb-10">Tweets</Heading>
+          <Heading>Tweets</Heading>
+          <p className="mt-8 mb-16 text-lg">
+            When you're in search for useful tips and tricks, tech Twitter is a perfect source.
+          </p>
         </Container>
         <Suspense fallback="loading...">
           {/* @ts-expect-error Server Component */}
@@ -28,7 +36,10 @@ const Resources = () => {
 
       <section className="min-h-screen py-40">
         <Container className="px-8">
-          <Heading className="mb-10">Articles</Heading>
+          <Heading>Articles</Heading>
+          <p className="mt-8 mb-16 text-lg">
+            Varying from case studies to tutorials and in depth knowledge.
+          </p>
         </Container>
         <Suspense fallback="loading...">
           {/* @ts-expect-error Server Component */}
@@ -38,7 +49,10 @@ const Resources = () => {
 
       <section className="min-h-screen py-40 bg-slate-900">
         <Container className="px-8">
-          <Heading className="mb-10">CodePens/CodeSandboxes</Heading>
+          <Heading>CodePens/CodeSandboxes</Heading>
+          <p className="mt-8 mb-16 text-lg">
+            People on the internet build the most incredible things.
+          </p>
         </Container>
         <Suspense fallback="loading...">
           {/* @ts-expect-error Server Component */}
