28a24830c8a83e4cbacf3686f42cbd6ab85ce20c
Ronny <4519142+rnnyrk@users.noreply.github.com>
2023-04-17 12:45:30 +0200

    Style experience page in new color scheme

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -10,36 +10,44 @@ export const metadata = {
 
 const Cv = () => {
   return (
-    <Container>
-      <Container className="mb-20">
+    <>
+      <Container className="mb-10 pb-20">
         <Heading>Experience</Heading>
-        <p className="mt-8 mb-20 text-lg">Work experience and projects I've been working on.</p>
+        <p className="mt-8 mb-20 text-lg">
+          After (and during) studying Comminucations and Multimedia Design at the Hogeschool van
+          Amsterdam, I've ended up working for great employers and got the opportunity to work on
+          beautiful projects.
+        </p>
       </Container>
 
-      <Experience
-        title="Label A"
-        date="March 2016 - present"
-        description="Creating digital products for clients as Amdax, Alicia Insurances, Fastis, Jobner, Nationale Nederlanden, Porsche Nederland"
-      />
+      <section className="py-40 bg-white">
+        <Container>
+          <Experience
+            title="Label A"
+            date="March 2016 - present"
+            description="Creating digital products for clients as Amdax, Alicia Insurances, Fastis, Jobner, Nationale Nederlanden, Porsche Nederland"
+          />
 
-      <Experience
-        title="Just Right Webdesign"
-        date="April 2012 - present"
-        description="Consulting for and creating digital products"
-      />
+          <Experience
+            title="Just Right Webdesign"
+            date="April 2012 - present"
+            description="Consulting for and creating digital products"
+          />
 
-      <Experience
-        title="BIT Students"
-        date="December 2016 - August 2018"
-        description="Working on various projects developing with VueJS, Polymer, ReactJS, iOS AR Kit"
-      />
+          <Experience
+            title="BIT Students"
+            date="December 2016 - August 2018"
+            description="Working on various projects developing with VueJS, Polymer, ReactJS, iOS AR Kit"
+          />
 
-      <Experience
-        title="IQ Media"
-        date="February 2013 - July 2014"
-        description="Full stack intern and employee. Mainly working with Javascript/jQuery and PHP"
-      />
-    </Container>
+          <Experience
+            title="IQ Media"
+            date="February 2013 - July 2014"
+            description="Full stack intern and employee. Mainly working with Javascript/jQuery and PHP"
+          />
+        </Container>
+      </section>
+    </>
   );
 };
 
