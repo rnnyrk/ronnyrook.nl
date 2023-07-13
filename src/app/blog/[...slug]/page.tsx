70432bb653bba1b4f4199caf5822f21cd3e0fd02
Ronny Rook <info@ronnyrook.nl>
2023-07-13 11:34:23 +0200

    Add ol/ul styling in markdown, dynamic meta data

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -3,9 +3,16 @@ import { Container } from 'common/layout/Container';
 import { Heading } from 'common/typography/Heading';
 import { Article } from 'modules/blog/Article';
 
-export const metadata = {
-  title: 'Blog',
-};
+export async function generateMetadata({ params }) {
+  const post = fetchPostBySlug(params.slug);
+
+  return {
+    title: post.title,
+    description: post.summary,
+    keywords: post.tags,
+    authors: [{ name: 'Ronny Rook', url: 'https://ronnyrook.nl' }],
+  };
+}
 
 const BlogPost = ({ params }) => {
   const post = fetchPostBySlug(params.slug);
@@ -13,11 +20,8 @@ const BlogPost = ({ params }) => {
   return (
     <>
       <Container className="px-8 mb-6 pb-10 md:mb-10 md:pb-20">
-        <Heading>Blog</Heading>
-        <p className="mt-8 mb-20 text-lg">
-          Every now and then I write a blog post about something I have a good opinion about or I
-          want to learn you something new.
-        </p>
+        <Heading>{post.title}</Heading>
+        <p className="mt-8 mb-20 text-lg">{post.summary}</p>
       </Container>
 
       <section className="py-20 md:py-40 bg-white dark:bg-rnny-dark-tint">
